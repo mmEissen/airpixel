7d8525a0207970f5c6c33964702f3e969c59eefb
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2018-10-24 21:23:22 +0100

    Add travis configuration for auto deployment
    This *should* deploy on all pushes to master...

--- setup.py
+++ setup.py
@@ -3,12 +3,21 @@ from setuptools import setup
 
 setup(
     name="Airpixel",
-    version="0.1.0",
-    url="",
+    version="0.1.1",
+    url="https://github.com/mmEissen/airpixel",
     author="Moritz Eissenhauer",
     author_email="moritz.eissenhauer@gmail.com",
     description="",
     packages=["airpixel"],
     install_requires=["numpy"],
-    extras_require={"mock": ["PyQt5"], "dev": ["pytest", "pylint", "mypy", "black"]},
+    extras_require={
+        "mock": ["PyQt5"],
+        "dev": ["pytest", "pylint", "mypy", "black", "twine"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )

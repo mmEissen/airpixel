928bd3764d4f0036c510df9332b3433fce8358fa
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2018-11-05 23:13:24 +0000

    add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+from os import path
 from setuptools import setup
+
+
+def load_long_descriprion():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, "README.rst")) as readme:
+        return readme.read()
 
 
 setup(
     name="Airpixel",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/mmEissen/airpixel",
     author="Moritz Eissenhauer",
     author_email="moritz.eissenhauer@gmail.com",
@@ -20,4 +27,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    long_description=load_long_descriprion(),
+    long_description_content_type="text/x-rst",
 )

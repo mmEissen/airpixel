1de88295d77854ae88bf336327a9086f01528b62
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2019-03-02 13:33:42 +0000

    remove size restriction

--- ESP8266airpixel/config_template.h
+++ ESP8266airpixel/config_template.h
@@ -1,21 +1,25 @@
 // Rename this file to `config.h`, edit the values, and place it next to the airpixel sketch
 
-
-// Set to 1 for debug output on the serial port
-// Set to 0 for improved performance (roughly x6 improvement in maximum refresh rate)
-#define DEBUG_MODE 1
-
-// Baud rate for debug output. Depending on your arduino device you may need to change this.
-// If DEBUG_MODE is disabled then you can completely ignore this value.
-#define SERIAL_BAUD 115200
-
-// Replace this with the number of LEDs you have in your setup
-#define NUM_LEDS 60
-
-// For most cases you can leave the part as is. You may need to use a different port if
+// For most cases you can leave the port as is. You may need to use a different port if
 // this one is already in use on your device.
 #define PORT 50000
 
 // Replace with the credentials for your WiFi
 #define WIFI_NAME "<Your WiFi SSID>"
 #define PASSWORD "<The password for your WiFi>"
+
+// Set to 1 for debug output on the serial port
+// Set to 0 for improved performance (roughly x6 improvement in maximum refresh rate)
+#define DEBUG_MODE 1
+
+// Baud rate for debug output. Depending on your arduino device you may need to change this.
+// If DEBUG_MODE is disabled then you can ignore this value.
+#define SERIAL_BAUD 115200
+
+// The size of the buffer alocated to the LEDs in bytes. This determines the maximum number
+// of LEDs that can be controlled by the controller. The maximum number of LEDs is:
+//   - (LED_BUFFER_SIZE / 4) for 4 color LEDs (RGBW or GRBW)
+//   - (LED_BUFFER_SIZE / 3) for 3 color LEDs (RGB or GRB)
+//
+// There is no significant advantage to a smaller buffer, other than reduced memory usage
+#define LED_BUFFER_SIZE 260000

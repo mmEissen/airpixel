ba9fceeb10a73d5ac48b507b7c33c78dfb09cdc7
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2019-04-01 19:15:57 +0100

    Send only latest message from buffer
    If sending takes too long then the buffer can fill up. Only the
    latest message is ever relevant.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def load_long_descriprion():
 
 setup(
     name="Airpixel",
-    version="0.6.4",
+    version="0.7.0",
     url="https://github.com/mmEissen/airpixel",
     author="Moritz Eissenhauer",
     author_email="moritz.eissenhauer@gmail.com",
